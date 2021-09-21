1a13f298f929101c70be27beab9c908037743e29
JooYoung <jykim0747@gmail.com>
2021-09-22 01:07:45 +0900

    [#7] rollback binary matrix multiplication

--- matrix/bmatrix.c
+++ matrix/bmatrix.c
@@ -438,15 +438,22 @@ int bmatrix_mul(BMAT dst, BMAT src1, BMAT src2)
         res = -2;
         goto end;
     }
-
+    printf("r, src1 cnum: %d, %d\n", r, src1->cnum);
     for(i=0; i<src1->r; ++i){
-        for(j=0; j<src1->cnum; ++j){
+        for(j=0; j<src1->cnum-1; ++j){
             for(k=0; k<8; ++k){
                 if(((b_mat_entry(src1, i, j)>>(7-k)) & 0x01) == 1){
                 for(l=src2->cnum-1; l>=0; l--){
                     b_mat_entry(dst, i, l) ^= b_mat_entry(src2, j*8 + k, l);
                 }
                 }
+            }
+        }
+        for(k=0; k<r; ++k){
+            if(((b_mat_entry(src1, i, src1->cnum-1)>>(7-k)) & 0x01) == 1){
+            for(l=src2->cnum-1; l>=0; l--){
+                b_mat_entry(dst, i, l) ^= b_mat_entry(src2, (src1->cnum-1)*8 + k, l);
+            }
             }
         }
     }
